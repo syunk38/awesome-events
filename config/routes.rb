600072ccf9364b17238b131105e875a6206fbde4
Syun <syunk38@gmail.com>
2017-01-22 18:42:40 +0900

    event作成form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  resources :events
   root to: 'welcome#index'
-
   get '/auth/:provider/callback' => 'sessions#create'
   get '/logout' => 'sessions#destroy', as: :logout
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
